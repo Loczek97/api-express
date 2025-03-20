e8c82c5f73d337c95a49f7ef85f9f0e7ba6b617e
Michał Szymański <szymanski.m698@gmail.com>
2025-03-20 14:21:20 +0100

    do Pelca na UML

--- index.js
+++ index.js
@@ -4,22 +4,22 @@ const { json } = require("body-parser");
 const cors = require("cors");
 const dbConnection = require("./db/dbConnection");
 const authRouter = require("./routes/authRouter");
-const postsRouter = require("./routes/postsRouter");
 
 const app = express();
 const port = process.env.PORT || 3000;
 
 app.use(json());
 app.use(accessLogger);
-app.use(cors({
-  origin: "*",
-  methods: ["GET", "POST", "PUT", "DELETE"],
-  allowedHeaders: ["Content-Type", "Authorization"],
-  credentials: true
-}))
+app.use(
+  cors({
+    origin: "*",
+    methods: ["GET", "POST", "PUT", "DELETE"],
+    allowedHeaders: ["Content-Type", "Authorization"],
+    credentials: true,
+  })
+);
 
 app.use("/auth", authRouter);
-app.use("/posts", postsRouter);
 
 dbConnection();
 app.listen(port, () => {
